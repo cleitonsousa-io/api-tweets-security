068e2141e1bba27ff84cdf25b04836c4e30f856e
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-11-27 08:40:39 -0300

    criação dos repositorios

---
+++ src/main/java/fcss/dev/security/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package fcss.dev.security.repository;
+
+import fcss.dev.security.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/fcss/dev/security/repository/TweetRepository.java
@@ -0,0 +1,9 @@
+package fcss.dev.security.repository;
+
+import fcss.dev.security.entities.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+}
