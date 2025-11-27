0e10b4b7de5c20078b30fb7b729aebd5bfc57b98
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-11-27 07:34:31 -0300

    criação da entity Tweet

---
+++ src/main/java/fcss/dev/security/entities/Tweet.java
@@ -0,0 +1,25 @@
+package fcss.dev.security.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_tweets")
+public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "tweet_id")
+    private Long tweetId;
+
+    private User user;
+
+    private String content;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+
+}
